dcf23aa08469828091b5b46966e78dd9d6ecaa04
Lewis <lmg3rd@gmail.com>
2019-07-23 11:46:53 -0400

    Initial Commit

---
+++ GamePicks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GamePicks
+//
+//  Created by Lewis Griffin on 7/23/19.
+//  Copyright © 2019 Lewis Griffin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
